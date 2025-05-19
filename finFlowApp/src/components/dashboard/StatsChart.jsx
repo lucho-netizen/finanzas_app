8ae654fdbb0132aef87d82e0aa2379421a81b425
Paco <114266465+lucho-netizen@users.noreply.github.com>
2025-05-18 19:01:19 -0500

    Beta 1.0.0.0.1.0

--- finFlowApp/src/components/dashboard/StatsChart.jsx
+++ finFlowApp/src/components/dashboard/StatsChart.jsx
@@ -1,22 +1,32 @@
-// src/components/dashboard/StatsChart.jsx
 import ChartContainer from "../common/ChartContainer";
-import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
+import {
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  CartesianGrid
+} from "recharts";
 
 export default function StatsChart({ data }) {
-  console.log("📊 Datos que llegan a StatsChart:", data);
+  console.log("Datos que llegan a StatsChart:", data);
 
   return (
     <ChartContainer>
       <h2 className="text-white mb-4">Estadísticas</h2>
       <ResponsiveContainer width="100%" height={300}>
         <BarChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="category" stroke="#ccc" />
           <YAxis stroke="#ccc" />
           <Tooltip />
-          <Bar dataKey="amount" fill="#4f46e5" radius={[4, 4, 0, 0]} />
+          <Legend />
+          <Bar dataKey="income" fill="#22c55e" name="Ingresos" />
+          <Bar dataKey="expense" fill="#ef4444" name="Egresos" />
         </BarChart>
       </ResponsiveContainer>
     </ChartContainer>
   );
 }
-
